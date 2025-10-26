63c6e6244124fdf5fde5f5746caed07532f395cb
iheboueslati909 <hooba500@gmail.com>
2025-10-26 13:43:48 +0100

    error response

---
+++ src/main/java/com/eventify/ms/dto/error/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.eventify.ms.dto.error;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponse(
+            LocalDateTime timestamp,
+            int status,
+            String error,
+            String message,
+            String path
+    ) {}
