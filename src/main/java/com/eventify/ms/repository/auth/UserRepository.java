3d1355a9678346b00675166c0160769b7f4e649b
iheboueslati909 <hooba500@gmail.com>
2025-10-26 23:58:20 +0100

    user repo ID fix

--- src/main/java/com/eventify/ms/repository/auth/UserRepository.java
+++ src/main/java/com/eventify/ms/repository/auth/UserRepository.java
@@ -2,7 +2,8 @@ package com.eventify.ms.repository.auth;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
-public interface UserRepository extends JpaRepository<com.eventify.ms.model.auth.User, Long> {
+import java.util.UUID;
+public interface UserRepository extends JpaRepository<com.eventify.ms.model.auth.User, UUID> {
   Optional<com.eventify.ms.model.auth.User> findByEmail(String email);
   boolean existsByEmail(String email);
 }
